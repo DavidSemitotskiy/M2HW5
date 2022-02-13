609f5756ffd558abe54bd8b1d45256c5d7cb36d1
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-02-13 17:43:11 +0200

    class Config was added

---
+++ M2HW5/LoggerConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2HW5
+{
+    public class LoggerConfig
+    {
+        public string DirectoryPath { get; set; }
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+    }
+}
